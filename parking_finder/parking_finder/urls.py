6388330889d4eaa706e85976ef451991568e4829
Harsh <63588817+wthxrsh@users.noreply.github.com>
2025-02-09 12:26:41 +0530

    Delete parking_finder directory

--- parking_finder/parking_finder/urls.py
+++
@@ -1,9 +0,0 @@
-# parking_finder/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # Admin panel
-    path('', include('parking.urls')),  # Include URLs from the parking app
-]
